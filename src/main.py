8c0ef501d4391f667eb3ee8cb5dd0ada59025406
esensible <esensible@users.noreply.github.com>
2023-04-26 08:34:15 +0930

    tidy up and recator

--- src/main.py
+++ src/main.py
@@ -11,18 +11,45 @@ import os
 import requests
 
 api_key = os.environ["GOOGLE_MAPS_API_KEY"]
-google_maps = f"https://maps.googleapis.com/maps/api/js?key={api_key}&callback=window.initMap"
+google_maps = (
+    f"https://maps.googleapis.com/maps/api/js?key={api_key}&callback=window.initMap"
+)
 
 app = dash.Dash(__name__)
 
-app.layout = html.Div([
-    dcc.Store(id="map-data"),
-    html.Div(id="map", style={"height": "500px", "width": "100%"}),
-    html.Button("Get Info", id="get-info-button"),
-    html.Label(id="info-label"),
-    html.Img(id="result"),
-])
+app.layout = html.Div(
+    [
+        dcc.Store(id="map-data"),
+        html.Div(
+            id="map", style={"height": "800px", "width": "100%", "position": "relative"}
+        ),
+        html.Div(
+            html.Button(
+                "Convert", id="get-info-button", style={"pointer-events": "auto"}
+            ),
+            id="floating-square",
+            style={
+                "border": "3px solid red",
+                "height": "640px",
+                "width": "640px",
+                "position": "absolute",
+                "top": "50%",
+                "left": "50%",
+                "transform": "translate(-50%, -50%)",
+                "pointer-events": "none",
+            },
+        ),
+        html.Img(id="result"),
+    ]
+)
 
+
+# lake bonney
+default_location = dict(
+    lat=-34.220359,
+    lon=140.4311491,
+    zoom=14,
+)
 
 app.clientside_callback(
     f"""
@@ -37,8 +64,8 @@ app.clientside_callback(
 
         function createMap() {{
             map = new google.maps.Map(document.getElementById('map'), {{
-                center: {{ lat: -34.397, lng: 150.644 }},
-                zoom: 8
+                center: {{ lat: {default_location["lat"]}, lng: {default_location["lon"]} }},
+                zoom: {default_location["zoom"]}
             }});
         }}
 
@@ -58,15 +85,15 @@ app.clientside_callback(
                     let zoom = map.getZoom();
                 
                     return {{lat: lat, lon: lng, zoom: zoom }}; 
-                    // `Lat: ${{lat}}, Lng: ${{lng}}, Zoom: ${{zoom}}`;
                 }}
                 return "";
         }};
     }})()
     """,
     Output("map-data", "data"),
-    Input("get-info-button", "n_clicks")
+    Input("get-info-button", "n_clicks"),
 )
+
 
 def download_google_maps_image(lat, lon, zoom, dimensions, api_key):
     map_type = "roadmap"
@@ -81,15 +108,20 @@ def download_google_maps_image(lat, lon, zoom, dimensions, api_key):
     if response.status_code == 200:
         # Load the image into an OpenCV buffer
         image_buffer = BytesIO(response.content)
-        image = cv2.imdecode(np.frombuffer(image_buffer.read(), np.uint8), cv2.IMREAD_COLOR)
+        image = cv2.imdecode(
+            np.frombuffer(image_buffer.read(), np.uint8), cv2.IMREAD_COLOR
+        )
         return image
     else:
         print(f"Error: Unable to download image. Status code: {response.status_code}")
         return None
 
-def extract_water_edge(image: np.ndarray, water_color: np.ndarray = np.array([249, 192, 156]), tolerance: int = 20) -> np.ndarray:
 
-
+def extract_water_edge(
+    image: np.ndarray,
+    water_color: np.ndarray = np.array([249, 192, 156]),
+    tolerance: int = 20,
+) -> np.ndarray:
     # Create a mask for the water region based on the color and tolerance
     lower_bound = np.clip(water_color - tolerance, 0, 255)
     upper_bound = np.clip(water_color + tolerance, 0, 255)
@@ -118,27 +150,29 @@ def extract_water_edge(image: np.ndarray, water_color: np.ndarray = np.array([24
     # # Set the line color to black (0, 0, 0) and the background to transparent
 
     rgba[:, :, 3] = (rgba[:, :, 0] > 0) * 255
-    rgba[(rgba[:, :, 0] > 0), :3] = (204, 204, 204) # (0, 0, 0)
+    rgba[(rgba[:, :, 0] > 0), :3] = (204, 204, 204)  # (0, 0, 0)
 
     return rgba
 
 
 @app.callback(Output("result", "src"), Input("map-data", "data"))
 def generate_map(map_data):
-
     if not map_data:
         raise PreventUpdate
 
-    img = download_google_maps_image(map_data["lat"], map_data["lon"], map_data["zoom"], (640, 640), api_key)
+    img = download_google_maps_image(
+        map_data["lat"], map_data["lon"], map_data["zoom"], (640, 640), api_key
+    )
 
     img = extract_water_edge(img)
 
     # Convert the image to a base64-encoded PNG using OpenCV
-    retval, buffer = cv2.imencode('.png', img)
-    img_str = base64.b64encode(buffer).decode('utf-8')
+    retval, buffer = cv2.imencode(".png", img)
+    img_str = base64.b64encode(buffer).decode("utf-8")
 
     # Return the base64-encoded image data
-    return f'data:image/png;base64,{img_str}'
+    return f"data:image/png;base64,{img_str}"
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run_server(debug=True)
